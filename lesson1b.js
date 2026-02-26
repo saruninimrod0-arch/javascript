a30b3592fe7ed46fc73e416c0f3be7d233437d40
saruninimrod0-arch <saruninimrod0@gmail.com>
2026-02-26 11:11:32 +0300

    add

---
+++ lesson1b.js
@@ -0,0 +1,11 @@
+// in javascript we need to have an initializer before declaring a variable. In ES6 we have two main types of initializers i.e:
+
+// let : it declares values which can be changed later on.
+
+let weight = 10.5
+
+console.log(weight)
+
+// const : It is used to declare variable which contains values that are not intended to be changed in entire program.
+const pi = 3.142
+console.log("The pi is:", pi)
---
+++ lesson1c.js
@@ -0,0 +1,2 @@
+// let : it declares values which can be changed later on.
+let height = 12.5
